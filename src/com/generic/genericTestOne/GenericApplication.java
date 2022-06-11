95e53932effeee3cb30f0a84d91bb61db31667eb
Dragon2623 <armin1.aliani1@gmail.com>
2022-06-11 13:13:10 +0430

    add GenericApplication

---
+++ src/com/generic/genericTestOne/GenericApplication.java
@@ -0,0 +1,10 @@
+package com.generic.genericTestOne;
+
+public class GenericApplication {
+    public static void main(String[] args) {
+        MyNonGenericClass myNonGenericClassObject = new MyNonGenericClass();
+        System.out.println("Show Integer was called! " + myNonGenericClassObject.showInteger(100));
+        System.out.println("Show String was called! " + myNonGenericClassObject.showString("Hi"));
+        System.out.println("Show Double was called! " + myNonGenericClassObject.showDouble(2.2));
+    }
+}
