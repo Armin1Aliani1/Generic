d1236e3c62b4883bac95fdda8c3ed9495f1845e6
Dragon2623 <armin1.aliani1@gmail.com>
2022-06-11 13:08:00 +0430

    edit MyNonGenericClass

--- src/com/generic/genericTestOne/MyNonGenericClass.java
+++ src/com/generic/genericTestOne/MyNonGenericClass.java
@@ -1,5 +1,19 @@
 package com.generic.genericTestOne;
 
 public class MyNonGenericClass {
+    public int showInteger(int i) {
+        return i;
+    }
 
+    public String showString(String str) {
+        return str;
+    }
+
+    public Double showDouble(Double d) {
+        return d;
+    }
+
+    public char showCharacter(Character ch) {
+        return ch;
+    }
 }
